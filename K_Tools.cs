63d7e90130e65452d85f02d512a6d25fdbc02717
Dawid Bieniek <dawid_b01@wp.pl>
2025-03-04 20:27:33 +0100

    Fix tools method to make library work

--- K_Tools.cs
+++ K_Tools.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-
-namespace Kociemba
+﻿namespace Kociemba
 {
+    // TODO: I should rewrie this library to make it clearer
     public class Tools
     {
+        public static readonly string TableDirectory;
+
+        private static readonly string AppdataDirectory;
+
+        static Tools()
+        {
+            AppdataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CubeRobot");
+            TableDirectory = Path.Combine(AppdataDirectory, "Tables");
+        }
+
         // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         // Check if the cube string s represents a solvable cube.
         // 0: Cube is solvable
@@ -15,18 +22,15 @@ namespace Kociemba
         // -4: Not all corners exist exactly once
         // -5: Twist error: One corner has to be twisted
         // -6: Parity error: Two corners or two edges have to be exchanged
-        // 
-        /// <summary>
-        /// Check if the cube definition string s represents a solvable cube.
-        /// </summary>
+        /// <summary> Check if the cube definition string s represents a solvable cube. </summary>
         /// <param name="s"> is the cube definition string , see <seealso cref="Facelet"/> </param>
         /// <returns> 0: Cube is solvable<br>
-        ///         -1: There is not exactly one facelet of each colour<br>
-        ///         -2: Not all 12 edges exist exactly once<br>
-        ///         -3: Flip error: One edge has to be flipped<br>
-        ///         -4: Not all 8 corners exist exactly once<br>
-        ///         -5: Twist error: One corner has to be twisted<br>
-        ///         -6: Parity error: Two corners or two edges have to be exchanged </returns>
+        /// -1: There is not exactly one facelet of each colour<br>
+        /// -2: Not all 12 edges exist exactly once<br>
+        /// -3: Flip error: One edge has to be flipped<br>
+        /// -4: Not all 8 corners exist exactly once<br>
+        /// -5: Twist error: One corner has to be twisted<br>
+        /// -6: Parity error: Two corners or two edges have to be exchanged </returns>
         public static int verify(string s)
         {
             int[] count = new int[6];
@@ -57,8 +61,11 @@ namespace Kociemba
         }
 
         /// <summary>
-        /// Generates a random cube. </summary>
-        /// <returns> A random cube in the string representation. Each cube of the cube space has the same probability. </returns>
+        /// Generates a random cube.
+        /// </summary>
+        /// <returns>
+        /// A random cube in the string representation. Each cube of the cube space has the same probability.
+        /// </returns>
         public static string randomCube()
         {
             CubieCube cc = new CubieCube();
@@ -74,60 +81,92 @@ namespace Kociemba
             return fc.to_fc_String();
         }
 
-
         // https://stackoverflow.com/questions/7742519/c-sharp-export-write-multidimension-array-to-file-csv-or-whatever
         // Kristian Fenn: https://stackoverflow.com/users/989539/kristian-fenn
-
         public static void SerializeTable(string filename, short[,] array)
         {
-            EnsureFolder("Assets\\Kociemba\\Tables\\");
-            BinaryFormatter bf = new BinaryFormatter();
-            Stream s = File.Open("Assets\\Kociemba\\Tables\\" + filename, FileMode.Create);
-            bf.Serialize(s, array);
-            s.Close();
+            EnsureTableDirectory();
+
+            using Stream s = File.Open(Path.Combine(TableDirectory, filename), FileMode.Create);
+            using BinaryWriter writer = new(s);
+
+            writer.Write(array.GetLength(0));
+            writer.Write(array.GetLength(1));
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    writer.Write(array[i, j]);
+                }
+            }
         }
 
         public static short[,] DeserializeTable(string filename)
         {
-            EnsureFolder("Assets\\Kociemba\\Tables\\");
-            Stream s = File.Open("Assets\\Kociemba\\Tables\\" + filename, FileMode.Open);
-            BinaryFormatter bf = new BinaryFormatter();
-            short[,] array = (short[,])bf.Deserialize(s);
-            s.Close();
+            EnsureTableDirectory();
+
+            using Stream s = File.Open(Path.Combine(TableDirectory, filename), FileMode.Open);
+            using BinaryReader reader = new(s);
+
+            int rowCount = reader.ReadInt32();
+            int colCount = reader.ReadInt32();
+
+            short[,] array = new short[rowCount, colCount];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    array[i, j] = reader.ReadInt16();
+                }
+            }
+
             return array;
         }
 
         public static void SerializeSbyteArray(string filename, sbyte[] array)
         {
-            EnsureFolder("Assets\\Kociemba\\Tables\\");
-            BinaryFormatter bf = new BinaryFormatter();
-            Stream s = File.Open("Assets\\Kociemba\\Tables\\" + filename, FileMode.Create);
-            bf.Serialize(s, array);
-            s.Close();
+            EnsureTableDirectory();
+
+            using Stream s = File.Open(Path.Combine(TableDirectory, filename), FileMode.Create);
+            using BinaryWriter writer = new(s);
+
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write(array[i]);
+            }
         }
 
         public static sbyte[] DeserializeSbyteArray(string filename)
         {
-            EnsureFolder("Assets\\Kociemba\\Tables\\");
-            Stream s = File.Open("Assets\\Kociemba\\Tables\\" + filename, FileMode.Open);
-            BinaryFormatter bf = new BinaryFormatter();
-            sbyte[] array = (sbyte[])bf.Deserialize(s);
-            s.Close();
+            EnsureTableDirectory();
+
+            using Stream s = File.Open(Path.Combine(TableDirectory, filename), FileMode.Open);
+            using BinaryReader reader = new(s);
+
+            int length = reader.ReadInt32();
+
+            sbyte[] array = new sbyte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = reader.ReadSByte();
+            }
+
             return array;
         }
 
         // https://stackoverflow.com/questions/3695163/filestream-and-creating-folders
         // Joe: https://stackoverflow.com/users/13087/joe
 
-        static void EnsureFolder(string path)
+        public static void EnsureTableDirectory()
         {
-            string directoryName = Path.GetDirectoryName(path);
-            // If path is a file name only, directory name will be an empty string
-            if (directoryName.Length > 0)
-            {
-                // Create all directories on the path that don't already exist
-                Directory.CreateDirectory(directoryName);
-            }
+            if (!Directory.Exists(AppdataDirectory))
+                Directory.CreateDirectory(AppdataDirectory);
+
+            if (!Directory.Exists(TableDirectory))
+                Directory.CreateDirectory(TableDirectory);
         }
-    }    
+    }
 }
